4e091e91efe9b5abd81d497a68986b4bff50ddc0
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-26 12:02:18 -0400

    Styling clean up

--- src/main/java/com/themastergeneral/pumpkinspice/PumpkinSpice.java
+++ src/main/java/com/themastergeneral/pumpkinspice/PumpkinSpice.java
@@ -1,7 +1,5 @@
 package com.themastergeneral.pumpkinspice;
 
-import com.themastergeneral.pumpkinspice.proxy.CommonProxy;
-
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -10,35 +8,34 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import com.themastergeneral.pumpkinspice.proxy.CommonProxy;
+
 @Mod(modid = PumpkinSpice.MODID, name = PumpkinSpice.MODNAME, version = PumpkinSpice.VERSION, dependencies = PumpkinSpice.DEPENDENCIES, updateJSON = PumpkinSpice.JSON)
-public class PumpkinSpice 
-{
+public class PumpkinSpice {
 	public static final String MODID = "pumpkinspice";
 	public static final String MODNAME = "Pumpkin Spice Latte";
-	public static final String VERSION = "1.0.0";
-	public static final String DEPENDENCIES = "required-after:ctdcore@[1.1.6,]";
+	public static final String VERSION = "1.1.0";
+	public static final String DEPENDENCIES = "required-after:ctdcore@[1.2,]";
 	public static final String JSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/PumpkinSpice.json";
-	
+
 	@Instance
-    public static PumpkinSpice instance = new PumpkinSpice();
-	
-	@SidedProxy(clientSide="com.themastergeneral.pumpkinspice.proxy.ClientProxy", serverSide="com.themastergeneral.pumpkinspice.proxy.ServerProxy")
+	public static PumpkinSpice instance = new PumpkinSpice();
+
+	@SidedProxy(clientSide = "com.themastergeneral.pumpkinspice.proxy.ClientProxy", serverSide = "com.themastergeneral.pumpkinspice.proxy.ServerProxy")
 	public static CommonProxy proxy;
-	
+
 	@EventHandler
-    public void preInit(FMLPreInitializationEvent e) 
-    {
-    	proxy.preInit(e);
-    }
-    @EventHandler
-    public void init(FMLInitializationEvent e) 
-    {
-    	proxy.init(e);
-    }
-        
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent e) 
-    {
-    	proxy.postInit(e);
-    }
+	public void preInit(FMLPreInitializationEvent e) {
+		proxy.preInit(e);
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent e) {
+		proxy.init(e);
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent e) {
+		proxy.postInit(e);
+	}
 }
--- src/main/java/com/themastergeneral/pumpkinspice/proxy/CommonProxy.java
+++ src/main/java/com/themastergeneral/pumpkinspice/proxy/CommonProxy.java
@@ -1,23 +1,21 @@
 package com.themastergeneral.pumpkinspice.proxy;
 
-import com.themastergeneral.pumpkinspice.items.ModItems;
-
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class CommonProxy 
-{
-	public void preInit(FMLPreInitializationEvent e) 
-    {
-		ModItems.init();
-    }
-    public void init(FMLInitializationEvent e) 
-    {
-    	
-    }
-    public void postInit(FMLPostInitializationEvent e) 
-    {
+import com.themastergeneral.pumpkinspice.items.ModItems;
 
-    }
+public class CommonProxy {
+	public void preInit(FMLPreInitializationEvent e) {
+		ModItems.init();
+	}
+
+	public void init(FMLInitializationEvent e) {
+
+	}
+
+	public void postInit(FMLPostInitializationEvent e) {
+
+	}
 }
