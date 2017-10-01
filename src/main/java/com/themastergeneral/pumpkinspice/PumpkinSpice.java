72f87c3dac67f32a4a9d4e884e2c9bf0b2e1190f
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-30 20:46:59 -0400

    Jar signed mod

--- src/main/java/com/themastergeneral/pumpkinspice/PumpkinSpice.java
+++ src/main/java/com/themastergeneral/pumpkinspice/PumpkinSpice.java
@@ -1,22 +1,25 @@
 package com.themastergeneral.pumpkinspice;
 
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import com.themastergeneral.pumpkinspice.proxy.CommonProxy;
 
-@Mod(modid = PumpkinSpice.MODID, name = PumpkinSpice.MODNAME, version = PumpkinSpice.VERSION, dependencies = PumpkinSpice.DEPENDENCIES, updateJSON = PumpkinSpice.JSON)
+@Mod(modid = PumpkinSpice.MODID, name = PumpkinSpice.MODNAME, certificateFingerprint = PumpkinSpice.FingerPrint, version = PumpkinSpice.VERSION, dependencies = PumpkinSpice.DEPENDENCIES, updateJSON = PumpkinSpice.JSON)
 public class PumpkinSpice {
 	public static final String MODID = "pumpkinspice";
 	public static final String MODNAME = "Pumpkin Spice Latte";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 	public static final String DEPENDENCIES = "required-after:ctdcore@[1.2,]";
 	public static final String JSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/PumpkinSpice.json";
+	public static final String FingerPrint = "441b509a0f58a0ef41aca8daf1be20d96287635e";
 
 	@Instance
 	public static PumpkinSpice instance = new PumpkinSpice();
@@ -37,5 +40,10 @@ public class PumpkinSpice {
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent e) {
 		proxy.postInit(e);
+	}
+
+	@EventHandler
+	public void onFingerprintViolation(FMLFingerprintViolationEvent e) {
+		FMLLog.warning("Invalid fingerprint detected for Pumpkin Spice Latte! TheMasterGeneral will not support this version!");
 	}
 }
