321981a7c2c47cfec330634c7331e38b452a6bb7
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-11-07 19:27:47 -0500

    Clean up this.

--- src/main/java/com/themastergeneral/pumpkinspice/LatteItem.java
+++ src/main/java/com/themastergeneral/pumpkinspice/LatteItem.java
@@ -2,10 +2,6 @@ package com.themastergeneral.pumpkinspice;
 
 import com.themastergeneral.ctdcore.item.CTDItem;
 
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
-
 public class LatteItem extends CTDItem {
 
 	public LatteItem(Properties properties, String name) {
