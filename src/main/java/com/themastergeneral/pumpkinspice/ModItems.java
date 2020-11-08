b7d254240aaf260705d34d75bd82f24a3547335c
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-11-07 19:28:00 -0500

    Make it so it gives you the effects now :)

--- src/main/java/com/themastergeneral/pumpkinspice/ModItems.java
+++ src/main/java/com/themastergeneral/pumpkinspice/ModItems.java
@@ -1,11 +1,17 @@
 package com.themastergeneral.pumpkinspice;
 
 import net.minecraft.item.Food;
-import net.minecraft.item.Foods;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 
 public class ModItems {
-	Food pumpkinspice = (new Food.Builder()).hunger(3).saturation(1.0F).build();
-	public static LatteItem latte = new LatteItem((new Item.Properties()).group(ItemGroup.FOOD).food(Foods.APPLE), "latte");
+	//@TODO: Yeah fix this.
+	static EffectInstance speed = new EffectInstance(Effects.SPEED, 10000, 4, true, false);
+	static EffectInstance jump = new EffectInstance(Effects.JUMP_BOOST, 10000, 1, true, false);
+	static EffectInstance haste = new EffectInstance(Effects.HASTE, 10000, 4, true, false);
+	
+	static Food pumpkinspice = (new Food.Builder()).hunger(3).saturation(1.0F).effect(speed, 1.0F).effect(jump, 1.0F).effect(haste, 1.0F).setAlwaysEdible().build();
+	public static LatteItem latte = new LatteItem((new Item.Properties()).group(ItemGroup.FOOD).food(pumpkinspice), "latte");
 }
