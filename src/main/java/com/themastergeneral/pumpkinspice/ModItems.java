073a42d131febb548c09d02abbd329d6a788c593
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-11-08 19:20:01 -0500

    Balance these out.

--- src/main/java/com/themastergeneral/pumpkinspice/ModItems.java
+++ src/main/java/com/themastergeneral/pumpkinspice/ModItems.java
@@ -35,9 +35,9 @@ import net.minecraft.potion.Effects;
 
 public class ModItems {
 	//@TODO: Yeah fix this.
-	static EffectInstance speed = new EffectInstance(Effects.SPEED, 10000, 4, true, false);
+	static EffectInstance speed = new EffectInstance(Effects.SPEED, 10000, 3, true, false);
 	static EffectInstance jump = new EffectInstance(Effects.JUMP_BOOST, 10000, 1, true, false);
-	static EffectInstance haste = new EffectInstance(Effects.HASTE, 10000, 4, true, false);
+	static EffectInstance haste = new EffectInstance(Effects.HASTE, 10000, 1, true, false);
 	
 	static Food pumpkinspice = (new Food.Builder()).hunger(3).saturation(1.0F).effect(speed, 1.0F).effect(jump, 1.0F).effect(haste, 1.0F).setAlwaysEdible().build();
 	public static LatteItem latte = new LatteItem((new Item.Properties()).group(ItemGroup.FOOD).food(pumpkinspice));
