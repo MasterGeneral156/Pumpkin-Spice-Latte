94280d352224752e6da6368ff3c49fbe62ce294e
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2025-05-05 09:51:34 -0400

    done

--- src/main/java/com/themastergeneral/pumpkinspice/ModItems.java
+++ src/main/java/com/themastergeneral/pumpkinspice/ModItems.java
@@ -38,7 +38,7 @@ public class ModItems {
 	static MobEffectInstance haste = new MobEffectInstance(MobEffects.DIG_SPEED, 10000, 1, true, false);
 	
 	//TODO Fix dep notice
-	static final FoodProperties pumpkinSpice = new FoodProperties.Builder().nutrition(4).saturationMod(0.1F).effect(speed, 1.0F).effect(jump, 1.0F).effect(haste, 1.0F).build();
+	static final FoodProperties pumpkinSpice = new FoodProperties.Builder().nutrition(4).saturationModifier(0.1F).effect(speed, 1.0F).effect(jump, 1.0F).effect(haste, 1.0F).build();
 	
 	public static LatteItem latte = new LatteItem((new Item.Properties()).food(pumpkinSpice));
 }
